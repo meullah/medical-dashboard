e868eae60bc0730be165cbc93ec5f1a8d863072e
SikandarZameer <sikandarzameer@gmail.com>
2020-01-04 15:54:29 +0500

    table

--- src/Components/Charts/patientChart.jsx
+++ src/Components/Charts/patientChart.jsx
@@ -1,16 +1,61 @@
 import React, { Component } from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 import { Line, Doughnut, Bar, Bubble } from "react-chartjs-2";
 import { Grid, Typography, ButtonGroup } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import Button from "@material-ui/core/Button";
 import CardContent from "@material-ui/core/CardContent";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 import Popup from "reactjs-popup";
 import "./popUp.css";
 import Select from "../Select/select";
 
+const StyledTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white
+  },
+  body: {
+    fontSize: 14
+  }
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.background.default
+    }
+  }
+}))(TableRow);
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9)
+];
+
+const classes = makeStyles({
+  table: {
+    minWidth: 700
+  }
+});
+
 export default class patientChart extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       open: false,
       openMorePopUp: false,
@@ -447,6 +492,40 @@ export default class patientChart extends Component {
                 />
               </CardContent>
             </Card>
+          </Grid>
+          <Grid item xs={12}>
+            <TableContainer component={Paper}>
+              <Table className={classes.table} aria-label="customized table">
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell>Date</StyledTableCell>
+                    <StyledTableCell align="right">Service</StyledTableCell>
+                    <StyledTableCell align="right">Doctor</StyledTableCell>
+                    <StyledTableCell align="right">Speciality</StyledTableCell>
+                    <StyledTableCell align="right">Relatives</StyledTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map(row => (
+                    <StyledTableRow key={row.name}>
+                      <StyledTableCell component="th" scope="row">
+                        {row.name}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {row.calories}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                      <StyledTableCell align="right">
+                        {row.carbs}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {row.protein}
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </Grid>
         </Grid>
 
